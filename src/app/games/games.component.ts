6e6001b0fae005e1efa5ec36852308034a0d217a
wesley <wesleyyw8@gmail.com>
2020-02-26 11:14:21 +0000

    first commit

---
+++ src/app/games/games.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  templateUrl: './games.component.html',
+  styleUrls: ['./games.component.less']
+})
+export class GamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

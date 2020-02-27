21a0b4720aecc3b43c1c96f7168089d40d45b5df
wesley <wesleyyw8@gmail.com>
2020-02-27 16:29:37 +0000

    animation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from './app.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
   title = 'malangular';
